a53bb7b390fe5da78304f3ed2662c5af0890b3eb
Gabriel Lima de Souza <90854484+GabrielLimaDeSouza@users.noreply.github.com>
2023-05-26 10:24:25 -0300

    Update Main.java

--- implementacao/app/Main.java
+++ implementacao/app/Main.java
@@ -1,15 +1,44 @@
 package implementacao.app;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import implementacao.source.ForcaBruta;
-import implementacao.source.Guloso;
 
 public class Main {
-    private static int[][] grafo; // matriz com os custos de viagem
-    private static int[] caminhoMinimo; // vetor com o caminho de menor custo
-    private static int menorCusto = Integer.MAX_VALUE; // custo mínimo encontrado
+    public static void main(String[] args) {
+        int vertices = 10; // Número de vértices do grafo
+        int[][] grafo = ForcaBruta.grafoCompletoPonderado(vertices);
 
+        imprimirMatriz(grafo);
 
+        ForcaBruta forcaBruta = new ForcaBruta();
+        forcaBruta.encontrarCaminhoMinimo(grafo);
+
+        List<Integer> caminhoMinimo = forcaBruta.getCaminhoMinimo();
+        int custoMinimo = forcaBruta.getMenorCusto();
+
+        System.out.println("Caminho de menor custo: ");
+        caminhoMinimo.forEach(vertice -> System.out.println(vertice));
+        System.out.println("\nCusto mínimo: " + custoMinimo);
+    }
+
+    private static void imprimirMatriz(int[][] matriz) {
+        int tamanho = matriz.length;
+
+        // Imprimir cabeçalho com os números das colunas
+        System.out.print("    ");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.printf("%2d ", i);
+        }
+        System.out.println();
+
+        // Imprimir linhas da matriz com números das linhas e elementos
+        for (int i = 0; i < tamanho; i++) {
+            System.out.printf("%2d: ", i);
+            for (int j = 0; j < tamanho; j++) {
+                System.out.printf("%2d ", matriz[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
